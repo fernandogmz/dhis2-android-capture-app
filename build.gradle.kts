008d346c49e6575ff914e6ecaebf936ab8ace69b
andresmr <andres@andresmr.com>
2026-01-29 14:52:06 +0100

    ci: update jacoco configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ allprojects {
             cacheChangingModulesFor(0, TimeUnit.SECONDS)
             eachDependency {
                 if (requested.group == "org.jacoco")
-                    useVersion("0.8.10")
+                    useVersion(libs.versions.jacoco.get())
                 // Force androidx.compose.animation to 1.9.5 to maintain compatibility with sharedElement API
                 if (requested.group == "androidx.compose.animation") {
                     useVersion("1.9.3")
